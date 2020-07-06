169fb3eaa77a60f03192500f8c4cb1840e0ca5b6
jmurga <jesus.murga@uab.cat>
2020-07-06 15:27:10 +0200

    abc-mk simulations

---
+++ scripts/src/sim.jl
@@ -0,0 +1,65 @@
+using Analytical, CSV,DataFrames, ProgressMeter
+
+function summStats(param::Analytical.parameters,iter::Int64,div::Array,sfs::Array,output::String,b::Int64,c::Float64)
+	# @threads
+	@showprogress for i in 1:iter
+	# for i in 1:iter
+
+
+		fac       = rand(-2:0.05:2)
+		afac      = 0.184*(2^fac)
+		bfac      = 0.000402*(2^fac)
+		
+		alTot     = rand(collect(0.05:0.05:0.4))
+		alLow     = rand(collect(0.0:0.05:alTot))
+
+		# println((thread=Threads.threadid(), iteration=i))
+		bgsIter(param,afac,bfac,alTot,alLow,div,sfs,output,b,c)
+	end
+end
+
+function bgsIter(param::Analytical.parameters,afac::Float64,bfac::Float64,alTot::Float64,alLow::Float64,div::Array,sfs::Array,output::String,b::Int64,c::Float64)
+
+	for j in param.bRange
+        # j = 0.999
+        param.al = afac; param.be = bfac; 
+        param.alLow = alLow; param.alTot = alTot; param.B = j
+
+        Analytical.set_theta_f(param)
+        theta_f = param.theta_f
+        param.B = 0.999
+        Analytical.set_theta_f(param)
+        Analytical.setPpos(param)
+        param.theta_f = theta_f
+        param.B = j
+        # x,y = Analytical.analyticalAlpha(param=param)
+        x,y,z = Analytical.alphaByFrequencies(param,div,sfs,b,c)
+
+
+        # println(z)
+
+        Analytical.summaryStatistics(output, z)
+
+		end
+end
+
+
+
+
+sfs = convert(Array,DataFrame!(CSV.File("/home/jmurga/mkt/202004/rawData/simulations/noBgsNoDemog/sfsBgs.tsv")))
+sfs = convert.(Int64,Analytical.cumulativeSfs(sfs))
+
+sfsPos = [sfs[:,1] + sfs[:,2] sfs[:,1] + sfs[:,2]]
+sfsNopos = [sfs[:,4] + sfs[:,2] sfs[:,4] + sfs[:,2]]
+
+div = convert(Array,DataFrame!(CSV.File("/home/jmurga/mkt/202004/rawData/simulations/noBgsNoDemog/divBgs.tsv")))
+anDiv = [convert(Int64,sum(div)),convert(Int64,sum(div))]
+
+# Set up model
+adap = Analytical.parameters(N=500,n=661, gam_neg=-457, gL=10,gH=500,B=0.999,alTot=0.4,alLow=0.4)
+#adap.nn=101
+Analytical.binomOp(adap)
+
+summStats(adap,1,anDiv,sfsPos,"/home/jmurga/mkt/202004/rawData/summStat/test",100,0.999)
+summStats(adap,parse(Int,ARGS[1]),anDiv,sfsPos,"/home/jmurga/mkt/202004/rawData/summStat/" * ARGS[2] , 100,0.999)
+
